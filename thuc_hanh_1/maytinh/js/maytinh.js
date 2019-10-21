dd4c7dbeef6da513792ac4d63b92ae04f327cb32
Le Huu Nhan <lehuunhan529@gmail.com>
2019-10-21 15:32:10 +0700

    Thuc_Hanh_1

---
+++ thuc_hanh_1/maytinh/js/maytinh.js
@@ -0,0 +1,114 @@
+$('.button-rad').click(function() {
+	$('#result').html('Rad');
+});
+
+$('.button-inv').click(function() {
+	$('#result').html('Inv');
+});
+
+$('.button-pi').click(function() {
+	$('#result').html('&#960');
+});
+
+$('.button-e').click(function() {
+	$('#result').html('e');
+});
+
+$('.button-ans').click(function() {
+	$('#result').html('Ans');
+});
+$('.button-deg').click(function() {
+	$('#result').html('Deg');
+});
+$('.button-sin').click(function() {
+	$('#result').html('Sin');
+});
+$('.button-cos').click(function() {
+	$('#result').html('Cos');
+});
+$('.button-tan').click(function() {
+	$('#result').html('tan');
+});
+$('.button-exp').click(function() {
+	$('#result').html('exp');
+});
+
+$('.button-x').click(function() {
+	$('#result').html('x!');
+});
+$('.button-ln').click(function() {
+	$('#result').html('ln');
+});
+$('.button-log').click(function() {
+	$('#result').html('log');
+});
+$('.button-can').click(function() {
+	$('#result').html('&#8730');
+});
+$('.button-mu').click(function() {
+	$('#result').html('^');
+});
+
+$('.button-ngoac-trai').click(function() {
+	$('#result').html('(');
+});
+$('.button-7').click(function() {
+	$('#result').html('7');
+});
+$('.button-4').click(function() {
+	$('#result').html('4');
+});
+$('.button-1').click(function() {
+	$('#result').html('1');
+});
+$('.button-0').click(function() {
+	$('#result').html('0');
+});
+
+$('.button-ngoac-phai').click(function() {
+	$('#result').html(')');
+});
+$('.button-8').click(function() {
+	$('#result').html('8');
+});
+$('.button-5').click(function() {
+	$('#result').html('5');
+});
+$('.button-2').click(function() {
+	$('#result').html('2');
+});
+$('.button-cham').click(function() {
+	$('#result').html('.');
+});
+
+$('.button-phan-tram').click(function() {
+	$('#result').html('%');
+});
+$('.button-9').click(function() {
+	$('#result').html('9');
+});
+$('.button-6').click(function() {
+	$('#result').html('6');
+});
+$('.button-3').click(function() {
+	$('#result').html('3');
+});
+$('.button-bang').click(function() { // xuat ket qua tai day
+	$('#result').html('=');
+});
+
+$('.button-ce').click(function() {  // reset lai bang
+	$('#result').html('CE');
+});
+$('.button-chia').click(function() {
+	$('#result').html('/');
+});
+$('.button-nhan').click(function() {
+	$('#result').html('*');
+});
+$('.button-tru').click(function() {
+	$('#result').html('-');
+});
+$('.button-cong').click(function() {
+	$('#result').html('+');
+});
